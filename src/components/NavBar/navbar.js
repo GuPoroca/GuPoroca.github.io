baff567513568f43c1b2895b2209d3d4a392db51
Poroca <gustavoporoca@gmail.com>
2024-03-24 21:41:00 -0300

    offset

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             <img src={logo} alt="Logo" className='logo'/>
             <div className="desktopMenu">
                 <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
-                <Link activeClass='active' to='about' spy={true} smooth={true} offset={-180} duration={500} className="desktopMenuListItem">Sobre</Link>
+                <Link activeClass='active' to='about' spy={true} smooth={true} offset={-40} duration={500} className="desktopMenuListItem">Sobre</Link>
                 <Link activeClass='active' to='projetos' spy={true} smooth={true} offset={-60} duration={500} className="desktopMenuListItem">Projetos</Link>
                 <Link activeClass='active' to='contatos' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Contatos</Link>
             </div>
